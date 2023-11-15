3b6907ec7f51338c1b20c30277f207936afe9622
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-15 12:34:54 -0800

    Added toggleClass, remove, and fadeOut, update addListItem

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -1,19 +1,65 @@
-/**
- * Toggles "done" class on <li> element
- */
+$(document).ready(function () {
+	/**
+	 * Toggles "done" class on <li> element
+	 */
+	// Create callback function to toggle class
+	function listToggleClass() {
+		let $this = $(this);
+		$this.toggleClass("done")
+	};
 
-/**
- * Delete element when delete link clicked
- */
+	// Execute callback function for list items
+	$("li").on("click", listToggleClass)
 
-/**
- * Adds new list item to <ul>
- */
-const addListItem = function(e) {
-  e.preventDefault();
-  const text = $('input').val();
 
-  // rest here...
-};
+	/**
+	 * Delete element when delete link clicked
+	 */
+	const $delete = $(".delete");
 
-// add listener for add
+	// Create callback function to remove items
+	function deleteItems(e) {
+		let $this = $(this);
+		// Add fade out [does not remove]
+		$this.parent().fadeOut("normal", function () {
+			$this.parent().remove();
+		})
+		e.stopPropagation()
+	};
+
+	// Execute callback function for delete
+	$delete.on("click", deleteItems);
+
+
+	/**
+	 * Adds new list item to <ul>
+	 */
+	const addListItem = function (e) {
+		e.preventDefault();
+		const text = $('input').val();
+
+		// Add new span item wth text
+		const $newSpan = $("<span>").text(text);
+
+		// Create new delete button with class and text
+		const $newDel = $("<a>").addClass("delete").text("Delete");
+
+		//Append span and delete to newly created list item
+		const $newListItem = $("<li>").append($newSpan, $newDel);
+
+		//Append item to current list
+		$("ul").append($newListItem);
+
+		$newListItem.on("click", listToggleClass);
+		$newDel.on("click", deleteItems);
+
+		// Reset input value
+		$('input').val(null);
+	};
+
+
+	// Add listener for add
+	const $add = $(".add-item");
+	$add.on("click", addListItem);
+
+});
