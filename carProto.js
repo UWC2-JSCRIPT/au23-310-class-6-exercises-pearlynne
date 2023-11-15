1f1c5ecbc1918cd279000b3b14cfbf3afac955d5
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-14 21:03:04 -0800

    Add solutions to in-class exercises

--- carProto.js
+++ carProto.js
@@ -6,6 +6,29 @@
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
 
+class Car {
+	constructor(model) {
+		this.currentSpeed = 0;
+		this.model = model;
+	}
+
+	accelerate() {
+		this.currentSpeed += 1;
+	}
+	brake() {
+		this.currentSpeed -= 1;
+	}
+
+	toString() {
+		return `${this.model} has a current speed of ${this.currentSpeed}mph.`
+	}
+}
+const qashQai = new Car("Qashqai");
+qashQai.accelerate();
+qashQai.accelerate();
+qashQai.brake();
+console.log(qashQai.toString());
+
 /**
  * ElectricCar class
  * @constructor
@@ -13,3 +36,27 @@
  */
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+
+class ElectricCar extends Car {
+	constructor(model) {
+		super(model);
+		this.motor = "electric";
+	}
+
+	// accelerate method that calls the inherited class accelerate
+	// method twice every time it is run 
+	accelerate() {
+		super.accelerate();
+		super.accelerate();
+	}
+
+	toString() {
+		return `Electric ${super.toString()}`;
+	}
+}
+
+const leaf = new ElectricCar("leaf");
+leaf.accelerate();
+leaf.accelerate();
+leaf.brake();
+console.log(leaf.toString());
