c5be9bc15db7284d8795471b9662d05cfd116547
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-15 11:00:51 -0800

    Add preventDefault and stopPropagation with jQuery

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,25 @@
 // Do not change
 document.getElementById('cat').addEventListener('click', () => {
-  alert('meow!');
+	alert('meow!');
 });
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+const $moreInfo = $('#more-info');
+
+$moreInfo.on("click", function (e) {
+	e.preventDefault();
+	alert("Here's some info");
+});
+
+
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+
+const $dog = $('#dog');
+
+$dog.on("click", function (e) {
+	alert("Bow wow!");
+	e.stopPropagation();
+});
